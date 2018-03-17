061ac2a551a6c02ec445bebb600236f7866071e1
Paul Cadman <git@paulcadman.io>
2018-03-17 13:37:00 +0000

    Initial commit of project

---
+++ Validated/Validated/Sources/AppDelegate.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        window.tintColor = UIColor(named: "teal")
+        window.rootViewController = UIViewController()
+        
+        return true
+    }
+
+}
+

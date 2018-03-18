039a22ea02ac8fa4644453cbbb65ec73c3b14712
Paul Cadman <git@paulcadman.io>
2018-03-18 17:44:39 +0000

    Refactor name field into a FormField protocol

--- Validated/Validated/Sources/Model/FormModel.swift
+++ Validated/Validated/Sources/Model/FormModel.swift
@@ -1,5 +1,38 @@
 import RxSwift
 
+enum FieldValid<T: Equatable>: Equatable {
+    case valid(value: T)
+    case invalid(value: T, reason: FormModel.Reason)
+}
+
+protocol FormField {
+    associatedtype T: Equatable
+    
+    var update: AnyObserver<T> { get }
+    var state: Observable<FieldValid<T>> { get }
+}
+
+struct Name: FormField {
+    typealias T = String
+
+    var state: Observable<FieldValid<T>>
+    var update: AnyObserver<T>
+    
+    private static let initialState: FieldValid<T> = .invalid(value: "", reason: .emptyName)
+    private var _state = BehaviorSubject<FieldValid<T>>(value: initialState)
+    
+    init() {
+        update = _state.mapObserver { input in
+            if input.isEmpty {
+                return .invalid(value: input, reason: .emptyName)
+            } else {
+                return .valid(value: input)
+            }
+        }
+        state = _state.asObservable().distinctUntilChanged()
+    }
+}
+
 struct FormModel {
     enum Reason {
         case emptyName
@@ -12,17 +45,18 @@ struct FormModel {
     
     var state: Observable<State>
     var updateName: AnyObserver<String>
-    
-    private var _state = BehaviorSubject<State>(value: .invalid(name: "", reason: .emptyName))
-    
+        
     init() {
-        updateName = _state.mapObserver { input in
-            if input.isEmpty {
-                return .invalid(name: input, reason: .emptyName)
-            } else {
-                return .valid(name: input)
+        let name = Name()
+        updateName = name.update
+        state = name.state.map { state in
+            switch state {
+            case .valid(let value):
+                return .valid(name: value)
+            case .invalid(let value, let reason):
+                return .invalid(name: value, reason: reason)
             }
+            
         }
-        state = _state.asObservable().distinctUntilChanged()
     }
 }

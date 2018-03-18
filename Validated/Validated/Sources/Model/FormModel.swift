76524f6f877bda686d03e50368e961136428dd06
Paul Cadman <git@paulcadman.io>
2018-03-18 16:41:35 +0000

    Add Observer to update name on the model

--- Validated/Validated/Sources/Model/FormModel.swift
+++ Validated/Validated/Sources/Model/FormModel.swift
@@ -7,14 +7,18 @@ struct FormModel {
     }
     
     var state: Observable<State>
+    var updateName: AnyObserver<String>
     
     private var _state = BehaviorSubject<State>(value: .invalid)
     
     init() {
-        state = _state.asObservable()
-    }
-    
-    func update() {
-        _state.onNext(.valid)
+        updateName = _state.mapObserver { input in
+            if input.isEmpty {
+                return .invalid
+            } else {
+                return .valid
+            }
+        }
+        state = _state.asObservable().distinctUntilChanged()
     }
 }
--- Validated/ValidatedTests/Sources/FormModelTests.swift
+++ Validated/ValidatedTests/Sources/FormModelTests.swift
@@ -7,4 +7,18 @@ class FormModelTests: XCTestCase {
         let model = FormModel()
         model.state.assert(startsWith: [.invalid]) { }
     }
+    
+    func testUpdateToNonEmptyStringIsValid() throws {
+        let model = FormModel()
+        model.state.assert(startsWith: [.invalid, .valid]) {
+            model.updateName.onNext("h")
+        }
+    }
+    
+    func testUpdateToEmptyStringRemainsInvalid() throws {
+        let model = FormModel()
+        model.state.assert(startsWith: [.invalid]) {
+            model.updateName.onNext("")
+        }
+    }
 }

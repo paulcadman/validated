93b171093689a671ca6ff7db86fcd9d50f7e4d6d
Paul Cadman <git@paulcadman.io>
2018-03-18 20:34:15 +0000

    Add a SwiftCheck test

--- Validated/ValidatedTests/Sources/FormFieldTests.swift
+++ Validated/ValidatedTests/Sources/FormFieldTests.swift
@@ -1,9 +1,19 @@
 import XCTest
+import SwiftCheck
 @testable import Validated
 
 class NameFormFieldTests: XCTestCase {
     func testEmptyStringIsInvalid() {
         let name = FormModel.Validators.name
         XCTAssertEqual(name(""), FieldState.invalid(value: "", reason: .emptyName))
+    }
+    
+    func testNonEmptyStringIsValid() {
+        let name = FormModel.Validators.name
+        property("All non-empty strings are valid") <- forAll { (s : String) in
+            return (!s.isEmpty) ==> {
+                return name(s) == FieldState.valid(value: s)
+            }
+        }
     }
 }
